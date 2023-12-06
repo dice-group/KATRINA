5ab66dc453b7d99eebca68c5501899425ca15d48
Daniel Vollmers <vdaniel@upb.de>
2023-12-06 11:20:04 +0100

    training with entities

--- GeneratorModel/train.py
+++ GeneratorModel/train.py
@@ -2,7 +2,7 @@ from parameters import KATRINAParser
 from typing import Callable, Dict, List, Optional, Tuple, Iterable
 import numpy as np
 import os
-from data_processing import ListDataset,LCqUAD, LCqUAD_rep_vars,LCqUAD_rep_vars_triples, Dataprocessor_Combined_simple
+from data_processing import ListDataset,LCqUAD, LCqUAD_rep_vars,LCqUAD_rep_vars_triples, Dataprocessor_Combined_simple,Dataprocessor_Combined_entities
 from transformers import Trainer
 from transformers import (
     AutoConfig,
@@ -101,7 +101,7 @@ def main():
         # compute_metrics_fn = summarization_metrics if "summarization" in task_name else translation_metrics
         compute_metrics_fn = exact_match_metrics
         return compute_metrics_fn
-    dg=Dataprocessor_Combined_simple(tokenizer, params)
+    dg=Dataprocessor_Combined_entities(tokenizer, params)
 
     # Get datasets
     if params["train_model"]:
